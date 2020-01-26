71d328d0d8e0b15ed539b63196dd85e03a6d0168
Krisztian Csepregi <krisztian.csepregi@benestudio.co>
2020-01-26 14:00:19 +0100

    Updates

---
+++ precache-manifest.d5ed78a26f890b4c46ca89921801a7e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d89793af09a14c9e5c05a7a2a3b7d469",
+    "url": "/ggg-movie-app/index.html"
+  },
+  {
+    "revision": "90b72a853c8dc25a7bbd",
+    "url": "/ggg-movie-app/static/css/main.6cd70849.chunk.css"
+  },
+  {
+    "revision": "0f9d74dd354b4fdf08d9",
+    "url": "/ggg-movie-app/static/js/2.d2650fa9.chunk.js"
+  },
+  {
+    "revision": "59c5b29d84d87bb3fd83b906dd4984bc",
+    "url": "/ggg-movie-app/static/js/2.d2650fa9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "90b72a853c8dc25a7bbd",
+    "url": "/ggg-movie-app/static/js/main.41daa1b3.chunk.js"
+  },
+  {
+    "revision": "e5254f83fb558a20292f",
+    "url": "/ggg-movie-app/static/js/runtime-main.49f3454c.js"
+  },
+  {
+    "revision": "7b7c83ca884e62b8373be4b89a2a49e3",
+    "url": "/ggg-movie-app/static/media/spinner.7b7c83ca.svg"
+  }
+]);
