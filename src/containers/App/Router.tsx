83bc6dff2f278a55db472a087dfbc9196d8d79df
Krisztian Csepregi <csepregi.krisztian@gmail.com>
2020-08-02 21:28:23 +0200

    fix: user hash router

--- src/containers/App/Router.tsx
+++ src/containers/App/Router.tsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 
 import Home from "../../pages/Home";
 import Movie from "../../pages/Asset";
 import Player from "../../pages/Player";
 
 const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
       <Route path="/player/:assetType/:id" component={Player} />
       <Route path="/:assetType/:id" component={Movie} />
       <Route component={Home} />
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default Router;
